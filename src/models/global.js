6f9175301cb4a302247e5fe11017a0c50983c631
YuChenLiao <yuchenliao3@gmail.com>
2021-07-06 20:08:12 +0800

    定义状态修改操作

--- src/models/global.js
+++ src/models/global.js
@@ -7,15 +7,52 @@ export default {
         double: [],
       },
     },
-    personInfo: {
+    personInfo: [],
+    postForm: {
       name: '',
       code: '',
-      count: '',
-      log: [],
+      type: '',
+      room: '',
+      phone: '',
     },
     login: false,
     token: '', // 不进行持久化保存和登陆状态记录
   },
   effect: {},
-  reducers: {},
+  reducers: {
+    initView(state, { payload }) {
+      return {
+        ...state,
+        roomInfo: payload.roomInfo,
+        personInfo: payload.personInfo,
+      };
+    },
+    changeRoom(state, { payload }) {
+      return {
+        ...state,
+        roomInfo: {
+          ...state.roomInfo,
+          ...payload,
+        },
+      };
+    },
+    changePerson(state, { payload }) {
+      return {
+        ...state,
+        personInfo: {
+          ...state.personInfo,
+          ...payload,
+        },
+      };
+    },
+    changePost(state, { payload }) {
+      return {
+        ...state,
+        postForm: {
+          ...state.postForm,
+          ...payload,
+        },
+      };
+    },
+  },
 };
